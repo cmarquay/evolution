850fee0f7fa14021cdf622433c2d22516aea5c9d
Foissac-Nicola <nfoissac@foissac.nicola>
2016-02-05 08:47:51 +0100

    ajout des classe competence

---
+++ Brevet_Evolution/src/Competence/EnumCompetence.java
@@ -0,0 +1,25 @@
+/*
+ * CC BY Project Brevet Evolution.
+ *
+ * Copyright CC BY 2016.
+ * Brillet Camille.
+ * Fillon Aurélien.
+ * Foissac Nicola.
+ * Hanotel Guillaume.
+ * Henry-Brûlé Raphaël.
+ * Marquay Christian.
+ *
+ * Creative Commons Attribution 3.0 France.
+ * http://creativecommons.org/licenses/by/3.0/fr/legalcode
+ *
+ */
+package Competence;
+
+/**
+ *
+ * @author nfoissac
+ */
+public enum EnumCompetence
+{
+    
+}
