fbc1f1ed4fb5b20bdd46db801861447174a7a522
ghanotel <guiguihanotel@orange.fr>
2016-02-18 00:52:26 +0100

    Ajout du cours Figures planes

--- Site Web/figures_planes.php
+++ Site Web/figures_planes.php
@@ -5,13 +5,224 @@
 ?>
 
 
+    <main id="CoursFiguresPlanes" class="Cours">
+    
+    
+    <h2>Propriété de Thalès</h2>
+        
+        
+    <h4>La théorème de Thalès</h4>
+        
+    <h6>Enoncé</h6>
+        
+    <p>A, B, M sont alignés et A, C, N sont alignés. Si les droites (<span class="italic">BC</span>) et (<span class="italic">MN</span>) sont parallèles, alors, <span class="frac"><sup><span class="italic">AM</span></sup><span>/</span><sub><span class="italic">AB</span></sub></span> = <span class="frac"><sup><span class="italic">AN</span></sup><span>/</span><sub><span class="italic">AC</span></sub></span> = <span class="frac"><sup><span class="italic">MN</span></sup><span>/</span><sub><span class="italic">BC</span></sub></span> .</p>
+        
+        
+        <p>On se trouve dans l'un des trois cas suivants :</p>
+        
+        <p><img src="images/figure_img1.png" alt="thales 1"/></p>
+        
+        <h6>Utilisations</h6>
+        
+        <p>- Calculer une longueur quand on a deux droites sécantes coupées par deux droites parallèles (voir exemple 1) ;</p>
+        
+        <p>- Prouver que deux droites ne sont pas parallèles (voir exemple 2).</p>
+        
+        
+       <h6>Exemple 1</h6> 
+        
+        
+        <div class="gauche">
+        <p>Enoncé : On considère la figure ci-contre, où A, B et C appartiennent à la droite (d), et D et E appartiennent à la droite (d’). Les droites (BD) et (CE) sont parallèles. On donne <span class="italic">AB</span> = 2 <span class="italic">cm</span>, <span class="italic">AD</span> = 2,8 <span class="italic">cm</span>, <span class="italic">AE</span> = 7 <span class="italic">cm</span> et <span class="italic">CE</span> =10 <span class="italic">cm</span> . Calculer AC et BD.</p>
+        
+        </div>
+        
+        
+        <p class="image"><img src="images/figure_img2.png" alt="thales 2"/></p>
+        
+        
+        <p>Solution : Les points D, A, E sont alignés et les points B, A, C sont alignés. Les droites (BD) et (CE) sont parallèles. D’après le théorème de Thalès : <span class="frac"><sup><span class="italic">AB</span></sup><span>/</span><sub><span class="italic">AC</span></sub></span> = <span class="frac"><sup><span class="italic">AD</span></sup><span>/</span><sub><span class="italic">AE</span></sub></span> = <span class="frac"><sup><span class="italic">BD</span></sup><span>/</span><sub><span class="italic">CE</span></sub></span>. On a alors <span class="frac"><sup>2</sup><span>/</span><sub><span class="italic">AC</span></sub></span> = <span class="frac"><sup>2,8</sup><span>/</span><sub>7</sub></span> = <span class="frac"><sup><span class="italic">BD</span></sup><span>/</span><sub>10</sub></span> .</p>
+        
+        
+        <p><span class="frac"><sup>2</sup><span>/</span><sub><span class="italic">AC</span></sub></span> = <span class="frac"><sup>2,8</sup><span>/</span><sub>7</sub></span></p>
+        
+        <p><span class="italic">AC</span> = <span class="frac"><sup>2 &times; 7</sup><span>/</span><sub>2,8</sub></span> = 5 <span class="italic">cm</span></p>
+        
+        <br/>
+        
+        <p><span class="frac"><sup>2,8</sup><span>/</span><sub>7</sub></span> = <span class="frac"><sup><span class="italic">BD</span></sup><span>/</span><sub>10</sub></span></p>
+        
+        <p><span class="italic">BD</span> = <span class="frac"><sup>2,8 &times; 10</sup><span>/</span><sub>7</sub></span> = 4 <span class="italic">cm</span></p>
+        
+        
+        <div class="gauche">
+        
+        <h6>Exemple 2</h6>
+        
+        <p>Enoncé : On considère la figure ci-contre.</p>
+        <p>On donne <span class="italic">OR</span> = 3 <span class="italic">cm</span>, <span class="italic">OT</span> = 4 <span class="italic">cm</span> , <span class="italic">RS</span> =1,5 <span class="italic">cm</span> et TU =1,8 cm . Les droites (RT) et (SU) sont-elles
+parallèles ?</p>
+        
+        
+        </div>
+        
+        <p class="image"><img src="images/figure_img3.png" alt="thales 3"/></p>
+        
+        
+        
+        <p>Solution : <span class="frac"><sup><span class="italic">OR</span></sup><span>/</span><sub><span class="italic">OS</span></sub></span> = <span class="frac"><sup>3</sup><span>/</span><sub>4,5</sub></span> &asymp; 0,67 et <span class="frac"><sup><span class="italic">OT</span></sup><span>/</span><sub><span class="italic">OU</span></sub></span> = <span class="frac"><sup>4</sup><span>/</span><sub>5,8</sub></span> &asymp; 0,69 donc <span class="frac"><sup><span class="italic">OR</span></sup><span>/</span><sub><span class="italic">OS</span></sub></span> &ne; <span class="frac"><sup><span class="italic">OT</span></sup><span>/</span><sub><span class="italic">OU</span></sub></span> . On en déduit que les droites (RT) et
+(SU) ne sont pas parallèles (sinon, d’après le théorème de Thalès, les deux quotients seraient égaux).</p>
+        
+        
+        
+     <h4>2) Réciproque du théorème de Thalès</h4>   
+        
+        
+        <h6>Enoncé</h6>
+        
+        <p>A, B, M sont alignés et A, C, N sont alignés. Si <span class="frac"><sup><span class="italic">AM</span></sup><span>/</span><sub><span class="italic">AB</span></sub></span> = <span class="frac"><sup><span class="italic">AN</span></sup><span>/</span><sub><span class="italic">AN</span></sub></span> et si les points A, B, M et les points A, C, N sont dans le même ordre, alors les droites (<span class="italic">BC</span>) et (<span class="italic">MN</span>) sont parallèles.</p>
+        
+        
+        <p>On se trouve dans l'un des trois cas suivants :</p>
+        
+        
+        <p><img src="images/figure_img4.png" alt="thales 4"/></p>
+        
+        
+        <h6>Utilisation : </h6>
+        
+        <p>Prouver que deux droites sont parallèles (voir exemple 3).</p>
+        
+        <h6>Exemple 3</h6>
+        
+    
+        <div class="gauche">
+        <p>Enoncé : (d) et (d') sont deux droites sécantes en A. B et C appartiennent à la droite (d'). D et E appartiennent à la droite (d). On donne : <span class="italic">AC</span> = 3 <span class="italic">cm</span> , <span class="italic">AB</span> = 5 <span class="italic">cm</span> , <span class="italic">AD</span> = 6,5 <span class="italic">cm</span> et <span class="italic">AE</span> = 3,9 <span class="italic">cm</span> . Les droites (BD) et (CE) sont-elles parallèles ?</p>
+        
+        </div>
+        
+        
+        <p class="image"><img src="images/figure_img5.png" alt="thales 5"/></p>
+        
+        <p>Solution : <span class="frac"><sup><span class="italic">AC</span></sup><span>/</span><sub><span class="italic">AB</span></sub></span> = <span class="frac"><sup>3</sup><span>/</span><sub>5</sub></span> = 0,6 et <span class="frac"><sup><span class="italic">AE</span></sup><span>/</span><sub><span class="italic">AD</span></sub></span> = <span class="frac"><sup>3,9</sup><span>/</span><sub>6,5</sub></span> = 0,6 donc <span class="frac"><sup><span class="italic">AC</span></sup><span>/</span><sub><span class="italic">AB</span></sub></span> = <span class="frac"><sup><span class="italic">AE</span></sup><span>/</span><sub><span class="italic">AD</span></sub></span>. B, A, C sont alignés et D, A, E sont alignés dans le même ordre. D'après la réciproque du théorème de Thalès, on en déduit que les droites (BD) et (CE) sont parallèles.</p>
+        
+        <br/>
+        <br/>
+        
+        
+        <h2>Trigonométrie</h2>
+        
+        <h6>Définitions</h6>
+        
+        <p>Si ABC est un triangle rectangle en C, on définit :</p>
+        
+        
+        <div class="gauche">
+            <ul class="liste_cours">
+                <li>Le cosinus de l'angle &ang;<span class="italic">BAC</span>, noté cos(<span class="italic">BAC</span>), le quotient : <br/>cos(<span class="italic">BAC</span>) = <span class="frac"><sup><span class="italic">AC</span></sup><span>/</span><sub><span class="italic">AB</span></sub></span> = <span class="frac"><sup><span class="italic">côté adjacent à &ang;BAC</span></sup><span>/</span><sub><span class="italic">hypoténuse</span></sub></span> ;</li>
+            
+                <li>Le sinus de l'angle &ang;<span class="italic">BAC</span>, noté sin(<span class="italic">BAC</span>), le quotient : <br/>sin(<span class="italic">BAC</span>) = <span class="frac"><sup><span class="italic">AC</span></sup><span>/</span><sub><span class="italic">AB</span></sub></span> = <span class="frac"><sup><span class="italic">côté opposé à &ang;BAC</span></sup><span>/</span><sub><span class="italic">hypoténuse</span></sub></span> ;</li>
+                
+                <li>La tangente de l'angle &ang;<span class="italic">BAC</span>, noté tan(<span class="italic">BAC</span>), le quotient : <br/>tan(<span class="italic">BAC</span>) = <span class="frac"><sup><span class="italic">AC</span></sup><span>/</span><sub><span class="italic">AB</span></sub></span> = <span class="frac"><sup><span class="italic">côté opposé à &ang;BAC</span></sup><span>/</span><sub><span class="italic">côté adjacent à &ang;BAC</span></sub></span> ;</li>
+                
+            </ul>
+        
+        </div>
+        
+        <p class="image"><img src="images/figure_img6.png" alt="trigo 1"/></p>
+        
+        
+        <h6>Exemple</h6>
+        
+        <div class="gauche">
+        <p>ABC est un triangle rectangle en B tel que <span class="italic">AB</span> = 5 <span class="italic">cm</span> et <span class="italic">&ang;BAC</span> = 33° .</p>
+        <p>Calculer <span class="italic">&ang;BCA</span>, BC et AC (on arrondira les
+longueurs au dixième).</p>
+        
+        
+        </div>
+        
+        <p class="image"><img src="images/figure_img7.png"/></p>
+        
+        <p>90 &minus; 33 = 57 donc <span class="italic">&ang;BCA</span> = 57° .</p>
+        <p>cos(<span class="italic">BAC</span>) = <span class="frac"><sup><span class="italic">AB</span></sup><span>/</span><sub><span class="italic">AC</span></sub></span> (car [AB] est le côté adjacent à l'angle <span class="italic">&ang;BAC</span> ).</p>
+        
+        <p>cos(33) = <span class="frac"><sup>5</sup><span>/</span><sub><span class="italic">AC</span></sub></span></p>
+        
+        <p><span class="italic">AC</span> &times; cos(33) = 5</p>
+        
+        <p><span class="italic">AC</span> = <span class="frac"><sup>5</sup><span>/</span>cos(33)</sub></span> &asymp; 6. [AC] mesure environ 6 cm.</p>
+
+        <p>tan(<span class="italic">&ang;BAC</span>) = <span class="frac"><sup><span class="italic">BC</span></sup><span>/</span><sub><span class="italic">AB</span></sub></span>  (car [BC] est le côté opposé à l'angle <span class="italic">&ang;BAC</span> et [AB] est le côté adjacent à l'angle
+<span class="italic">&ang;BAC</span> ).</p>
+
+        <p>tan(33) = <span class="frac"><sup><span class="italic">BC</span></sup><span>/</span><sub>5</sub></span></p>
+
+        <p><span class="italic">BC</span> = 5 &times; tan(33) &asymp; 3,2. [BC] mesure environ 3,2 cm.</p>
+        
+    
+
+<h6>Autre exemple</h6>
+
+
+<p>ABC est un triangle rectangle en C tel que <span class="italic">AB</span> = 7 <span class="italic">cm</span> et <span class="italic">BC</span> = 4 <span class="italic">cm</span> . Calculer la mesure des angles <span class="italic">&ang;BAC</span> et <span class="italic">&ang;ABC</span></p>
+
+<p>Dans le triangle ABC rectangle en C :</p>
+
+<div class="gauche">
+<p>sin(<span class="italic">&ang;BAC</span>) = <span class="frac"><sup><span class="italic">BC</span></sup><span>/</span><sub><span class="italic">AB</span></sub></span></p>
+
+<p>sin(<span class="italic">&ang;BAC</span>) = <span class="frac"><sup>4</sup><span>/</span><sub>7</sub></span></p>
+
+ <p>mes(<span class="italic">&ang;BAC</span>) = sin<sup>&minus;1</sup>(4:7) &asymp; 35°</p>   
+    
+</div>
+<p class="image"><img src="images/figure_img7.png"/></p>
+
+ <p>cos(<span class="italic">&ang;ABC</span>) = <span class="frac"><sup><span class="italic">BC</span></sup><span>/</span><sub><span class="italic">AB</span></sub></span></p>
+
+<p>cos(<span class="italic">&ang;ABC</span>) = <span class="frac"><sup>4</sup><span>/</span><sub>7</sub></span></p>
+
+ <p>mes(<span class="italic">&ang;ABC</span>) = cos<sup>&minus;1</sup>(4:7) &asymp; 55°</p>    
+    
+    
+<h6>Propriétés</h6>
+
+<p>Si <span class="italic">x</span> désigne la mesure d'un angle aigu, on a les relations suivantes :</p>
+
+<p>(cos <span class="italic">x</span>)<sup>2</sup> + (sin <span class="italic">x</span>)<sup>2</sup> = 1</p>
+
+<p>tan <span class="italic">x</span> = <span class="frac"><sup>sin <span class="italic">x</span></sup><span>/</span><sub>cos <span class="italic">x</span></sub></span></p>
+
+
+<p>Preuve :</p>
+
+
+<p>On se place dans un triangle ABC rectangle en B. Appelons <span class="italic">x</span> la mesure en degrés de l'angle <span class="italic">&ang;BCA</span> :</p>
+
+<p>sin <span class="italic">x</span> = <span class="frac"><sup><span class="italic">AB</span></sup><span>/</span><sub><span class="italic">AC</span></sub></span> , cos <span class="italic">x</span> = <span class="frac"><sup><span class="italic">BC</span></sup><span>/</span><sub><span class="italic">AC</span></sub></span> , tan <span class="italic">x</span> = <span class="frac"><sup><span class="italic">AB</span></sup><span>/</span><sub><span class="italic">BC</span></sub></span></p>
+
+<div class="gauche">
+
+<p>(sin <span class="italic">x</span>)<sup>2</sup> + (cos <span class="italic">x</span>)<sup>2</sup> =( <span class="frac"><sup><span class="italic">AB</span></sup><span>/</span><sub><span class="italic">AC</span></sub></span> )<sup>2</sup> + ( <span class="frac"><sup><span class="italic">BC</span></sup><span>/</span><sub><span class="italic">AC</span></sub></span> )<sup>2</sup> = <span class="frac"><sup><span class="italic">AB</span><sup>2</sup> + <span class="italic">BC</span><sup>2</sup></sup><span>/</span><sub><span class="italic">AC</span><sup>2</sup></sub></span> = <span class="frac"><sup><span class="italic">AC</span><sup>2</sup></sup><span>/</span><sub><span class="italic">AC</span><sup>2</sup></sub></span> = 1</p>
+
+<p>car <span class="italic">AB</span><sup>2</sup> + <span class="italic">BC</span><sup>2</sup> = <span class="italic">AC</span><sup>2</sup> d'après
+le théorème de Pythagore</p>
     
     
     
+</div>
+
+
+<p class="image"><img src="images/figure_img7.png" alt="trigo 2"</p>
+
+
+<p><span class="frac"><sup>sin <span class="italic">x</span></sup><span>/</span><sub>cos <span class="italic">x</span></sub></span> = <span class="frac"><sup><span class="italic">AB</span></sup><span>/</span><sub><span class="italic">AC</span></sub></span> = <span class="frac"><sup><span class="italic">AC</span></sup><span>/</span><sub><span class="italic">BC</span></sub></span> = <span class="frac"><sup><span class="italic">AB</span></sup><span>/</span><sub><span class="italic">BC</span></sub></span> = tan <span class="italic">x</span> .</p>
     
     
     
-</main>
+
+    </main>
 
 <?php
     include_once("footer.php");
