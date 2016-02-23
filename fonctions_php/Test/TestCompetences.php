83b1cc8ed60d4c1cfc5505b97c515010c0ffc990
Camille-Brillet <camille.brillet17@hotmail.com>
2016-02-23 18:11:37 +0100

    début du programme de test php

---
+++ fonctions_php/Test/TestCompetences.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * CC BY Project Brevet Evolution.
+ *
+ * Copyright CC BY 2016.
+ * Brillet Camille.
+ * Fillon Aurélien.
+ * Foissac Nicola.
+ * Hanotel Guillaume.
+ * Henry-Brûlé Raphaël.
+ * Marquay Christian.
+ *
+ * Creative Commons Attribution 3.0 France.
+ * http://creativecommons.org/licenses/by/3.0/fr/legalcode
+ *
+ */
+
+/**
+ *
+ * @author nfoissac
+ * @author cbrillet
+ */
+include "../Personnage/Personnage.php";
+include "../Personnage/Joueur.php";
+include "../Personnage/Monstre.php";
+include "../Competence/CompetenceJoueur.php";
+include "../Competence/CompetenceMonstre.php";
+include "../Statistique/Statistique.php";
+
+$test = new CompetenceJoueur;
+echo $test;
+System.out.println(Statistique.afficheStatistiqueCompetence());
+$test->donneCompetence(0)->gainNiveau();
+$test->donneCompetence(0)->ajouteExperience(5000);
+echo $test;
+echo Statistique::afficheStatistiqueCompetence();
+echo Statistique::afficheStatistiqueExperience();
+
+?>
