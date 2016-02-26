52578bd299c2466c00eb826aa654acdf70b79ad7
Guillaume Hanotel <Guillaume Hanotel>
2016-02-26 08:50:54 +0100

    index

--- Site Web/index.php
+++ Site Web/index.php
@@ -5,7 +5,7 @@
 <main id="index">
 
 
-
+<div id="index1"><p>Bienvenue sur Brevet Evolution, une plateforme éducative permettant de réviser vos cours de maths de 3ème et de vous tester à travers un quiz divertissant !</p></div>
 
 
 
