959f8f366211457015f9ee2553065e6989a080a7
guillaumehanotel <guiguihanotel@orange.fr>
2016-03-04 11:54:06 +0100

    Simplification menu

--- Site Web/header.php
+++ Site Web/header.php
@@ -66,14 +66,14 @@
     <!---- COURS ALGEBRE ---->
     <div id="SousListeAlgèbre" class="nav2">
         <ul>
-                <li id="NotionFonction"><a href="#">Notion de fonction</a></li>
-                <li id="FonctionLineaire"><a href="#">Fonction linéaire, fonction affine</a></li>
-                <li id="Statistique"><a href="#">Statistiques</a></li>
-                <li id="Probabilité"><a href="#">Probabilités</a></li>
-                <li id="Nombres"><a href="#">Nombres entiers et rationnels</a></li>
-                <li id="Calculs"><a href="#">Calculs élémentaires sur les radicaux</a></li>
-                <li id="Ecritures"><a href="#">Écritures littérales</a></li>
-                <li id="Equations"><a href="#">Équations et inéquations du premier degré</a></li>
+                <li id="NotionFonction"><a href="notion_fonction.php">Notion de fonction</a></li>
+                <li id="FonctionLineaire"><a href="fonction_affine_lineaire.php">Fonction linéaire, fonction affine</a></li>
+                <li id="Statistique"><a href="statistique.php">Statistiques</a></li>
+                <li id="Probabilité"><a href="probabilite.php">Probabilités</a></li>
+                <li id="Nombres"><a href="entiers_rationnels.php">Nombres entiers et rationnels</a></li>
+                <li id="Calculs"><a href="calculs_radicaux.php">Calculs élémentaires sur les radicaux</a></li>
+                <li id="Ecritures"><a href="ecritures_litterales.php">Écritures littérales</a></li>
+                <li id="Equations"><a href="equations_inequations.php">Équations et inéquations du premier degré</a></li>
         </ul>  
     </div>
     <!------------------------------>
@@ -84,138 +84,15 @@
     <!-- COURS GEOMETRIE -->
     <div id="SousListeGéométrie" class="nav2">
         <ul>
-                <li id="Figures"><a href="#">Figures planes</a></li>
-                <li id="ConfigurationEspace"><a href="#">Configurations dans l’espace</a></li>  
-                <li id="Aires"><a href="#">Aires et volumes</a></li>
-                <li id="Grandeurs"><a href="#">Grandeurs composées, changement d’unités</a></li>  
+                <li id="Figures"><a href="figures_planes.php">Figures planes</a></li>
+                <li id="ConfigurationEspace"><a href="configuration_espace.php">Configurations dans l’espace</a></li>  
+                <li id="Aires"><a href="aires_volumes.php">Aires et volumes</a></li>
+                <li id="Grandeurs"><a href="grandeurs_changements.php">Grandeurs composées, changement d’unités</a></li>  
         </ul>
     </div>
     
     
     
-    
-    
-     <!-------------------SOUS LISTE ALGEBRE---------------------------------->
-    
-    <div id="SousListeNotionFonction" class="nav3">
-        
-        <ul>
-            <h3>Notion de Fonction</h3>
-            <li><a href="notion_fonction.php">Cours</a></li>
-            <li><a href="#">Quiz</a></li>
-        </ul>
-    </div>
-    
-    <div id="SousListeFonctionsLineaire" class="nav3">
-        <ul>
-        <h3>Fonction linéaire, fonction affine</h3>
-        
-            <li><a href="fonction_affine_lineaire.php">Cours</a></li>
-            <li><a href="#">Quiz</a></li>
-        </ul>
-    </div>
-    
-    <div id="SousListeStatistique" class="nav3">
-        <ul>
-        <h3>Statistiques</h3>
-        
-            <li><a href="statistique.php">Cours</a></li>
-            <li><a href="#">Quiz</a></li>
-        </ul>
-    </div>
-    
-    <div id="SousListeProbabilité" class="nav3">
-        <ul>
-        <h3>Probabilités</h3>
-        
-            <li><a href="probabilite.php">Cours</a></li>
-            <li><a href="#">Quiz</a></li>
-        </ul>
-    </div>
-    
-    <div id="SousListeNombres" class="nav3">
-        <ul>
-        <h3>Nombres entiers et rationnels</h3>
-        
-            <li><a href="entiers_rationnels.php">Cours</a></li>
-            <li><a href="ER_quiz.php">Quiz</a></li>
-        </ul>
-    </div>
-    
-    <div id="SousListeCalculs" class="nav3">
-        <ul>
-        <h3>Calculs élémentaires sur les radicaux</h3>
-        
-            <li><a href="calculs_radicaux.php">Cours</a></li>
-            <li><a href="CER_quiz.php">Quiz</a></li>
-        </ul>
-    </div>
-    
-    <div id="SousListeEcritures" class="nav3">
-        <ul>
-        <h3>Ecritures littérales</h3>
-        
-            <li><a href="ecritures_litterales.php">Cours</a></li>
-            <li><a href="EL_quiz.php">Quiz</a></li>
-        </ul>
-    </div>
-    
-    <div id="SousListeEquations" class="nav3">
-        <ul>
-        <h3>Equations et inéquations du premier degré</h3>
-        
-            <li><a href="equations_inequations.php">Cours</a></li>
-            <li><a href="EI_quiz.php">Quiz</a></li>
-        </ul>
-    </div>
-        
-
-    
-    
-     <!----------------------------------------------------->
-    
-    
-    
-    <!-------------------SOUS LISTE GEOMETRIE---------------------------------->
-    
-    <div  id="SousListeFigures" class="nav3">
-        <ul>
-        <h3>Figures planes</h3>
-        
-            <li><a href="figures_planes.php">Cours</a></li>
-            <li><a href="#">Quiz</a></li>
-        </ul>
-   </div>
-    
-    <div  id="SousListeConfigurationEspace" class="nav3">
-        <ul>
-        <h3>Configurations dans l'espace</h3>
-        
-            <li><a href="configuration_espace.php">Cours</a></li>
-            <li><a href="#">Quiz</a></li>
-        </ul>
-    </div>
-    
-    <div  id="SousListeAires" class="nav3">
-        <ul>
-        <h3>Aires et volumes</h3>
-        
-            <li><a href="aires_volumes.php">Cours</a></li>
-            <li><a href="#">Quiz</a></li>
-        </ul>
-    </div>
-    
-    <div  id="SousListeGrandeurs" class="nav3">
-        <ul>
-        <h3>Grandeurs composées, changements d'unités</h3>
-        
-            <li><a href="grandeurs_changements.php">Cours</a></li>
-            <li><a href="#">Quiz</a></li>
-        </ul>
-    </div>
-    
-    
-    <!----------------------------------------------------->
     
     
 
