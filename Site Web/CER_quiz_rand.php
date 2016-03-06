6db72166760fa8505a00cb1910467e0a691d221d
Christian MARQUAY <cmarquay@etudiant.univ-lr.fr>
2016-03-06 11:01:01 +0100

    Raphael's random functions

---
+++ Site Web/CER_quiz_rand.php
@@ -0,0 +1,293 @@
+<?php
+//"√(-2 x -2) =",
+function question1(){
+	$rand=rand(1,10);
+	$enoce="√(-".($rand)." x -".($rand).") =";
+	$bonne=$rand;
+	
+	$mauvaise_list=array(
+		0=>"est impossible, la racine carrée n'est définie que pour les nombres positifs",
+		1=>($rand)."√(-".($rand).")",
+		2=>"√(-".($rand).") x √(-".($rand).")",
+		3=>$rand*2);
+	shuffle($mauvaise_list);
+	
+	$bonne_rep=rand(0,3);
+    
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//√(16/49) =
+function question2(){
+	$rand1=rand(1,10);
+	$rand2=rand(1,10);
+	$enoce="√(".($rand1*$rand1)."/".($rand2*$rand2).") =";
+	$bonne=$rand1/$rand2;
+	
+	$mauvaise_list=array(
+		0=>($rand2)."/".($rand1),
+		1=>($rand1)."/ √(".($rand2*$rand2).")",
+		2=>"√(-".($rand1).") x √(-".($rand2).")",
+		3=>$rand1/2 ."/".$rand2/2);
+	
+	shuffle($mauvaise_list);
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+//√(49 x 25) =
+function question3(){
+	$rand1=rand(1,10);
+	$rand2=rand(1,10);
+	$enoce="√(".($rand1*$rand1)."x".($rand2*$rand2).") =";
+	$bonne=$rand1*$rand2;
+	
+	$mauvaise_list=array(
+		0=>($rand2)."+".($rand1),
+		1=>($rand1)."x √(".($rand2*$rand2).")",
+		2=>"√(-".($rand1).") / √(-".($rand2).")",
+		3=>$rand1/2 ."*".$rand2/2);
+	
+	shuffle($mauvaise_list);
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//√(5929)² ="
+function question4(){
+	$rand=rand(1,100);
+	$enoce="√".($rand*$rand).")² =";
+	$bonne=$rand*$rand;
+	
+	$mauvaise_list=array(
+		0=>$rand,
+		1=>$rand*$rand/2,
+		2=>$rand*2);
+	shuffle($mauvaise_list);
+	
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//Si x² = 64, alors
+function question5(){
+	$rand=rand(1,10);
+	$enoce="i x² = ".($rand*$rand).", alors";
+	$bonne=$rand*$rand;
+	
+	$mauvaise_list=array(
+		0=>"l'équation admet une solution : x = ".($rand*$rand).,
+		1=>"l'équation n'a pas de solution",
+		2=>"l'équation admet une solution : x = "$rand,
+		3=>"l'équation admet une solution : x = -"$rand)
+	shuffle($mauvaise_list);
+	
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//√(20) x √(17) =
+function question6(){
+	$rand1=rand(1,50);
+	$rand2=rand(1,50);
+	$enoce="√(".($rand1)." x ".($rand2).") =";
+	$bonne="√".$rand1*$rand2;
+	
+	$mauvaise_list=array(
+		0=>($rand2/2)*($rand1/2),
+		1=>($rand1)."x √(".($rand2).")",
+		2=>($rand2)."x √(".($rand1).")");
+	
+	shuffle($mauvaise_list);
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//√(15) / √(28) =
+function question7(){
+	$rand1=rand(1,50);
+	$rand2=rand(1,50);
+	$enoce="√(".($rand1)." / ".($rand2).") =";
+	$bonne="√".$rand1/$rand2;
+	
+	$mauvaise_list=array(
+		0=>($rand2/2)*($rand1/2),
+		1=>($rand1)."/ √(".($rand2).")",
+		2=>($rand2)."/ √(".($rand1).")");
+	
+	shuffle($mauvaise_list);
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//1 / √(84)=a finir
+function question8(){
+	$rand1=rand(1,20);
+	$rand2=rand(1,20);
+	$enoce="1 / √".($rand1*$rand1*$rand2).") =";
+	$bonne="1/(".$rand1."√".$rand2.")";
+	
+	$mauvaise_list=array(
+		0=>($rand2/2)*($rand1/2),
+		1=>($rand1)."/ √(".($rand2).")",
+		2=>($rand2)."/ √(".($rand1).")");
+	
+	shuffle($mauvaise_list);
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//√(45) ="
+function question9(){
+	$rand1=rand(1,50);
+	$rand2=rand(1,50);
+	$enoce="√(".($rand1*$rand1*$rand2).") =";
+	$bonne=$rand1."√".$rand2;
+	
+	$mauvaise_list=array(
+		0=>($rand2/2)*($rand1/2),
+		1=>($rand1)."/ √(".($rand2).")",
+		2=>($rand2)."/ √(".($rand1).")",
+        3=>($rand2)."* √(".($rand1).")");
+	
+	shuffle($mauvaise_list);
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//"√(3/2) ="
+
+function question10(){
+	$rand1=rand(2,50);
+	$rand2=rand(2,50);
+	$enoce="√(".($rand1)."/".($rand2).") =";
+	$bonne="√".($rand1*$rand2)."/".$rand2;
+	
+	$mauvaise_list=array(
+		0=>($rand1*$rand1)."/".($rand2*$rand2),
+		1=>($rand2)."√(".($rand1)."/)".($rand2),
+		2=>($rand2)."/ √(".($rand1).")",
+        3=>($rand1)."* √(".($rand2).")");
+	
+	shuffle($mauvaise_list);
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+
+
+
+
+
+
+//print_r(question2());
+?>
---
+++ Site Web/ER_quiz_rand.php
@@ -0,0 +1,252 @@
+<?php
+
+//"(4/3)/(5/4) ="
+function question1(){
+	$rand=rand(1,20);
+    $rand1=rand(1,20);
+    $rand2=rand(1,20);
+    $rand3=rand(1,20);
+	$enoce="(".($rand)."/".($rand1).")/(".($rand2)."/".($rand3).") =";
+	$sup=$rand*$rand3;
+    $inf=$rand1*$rand2;
+    do{
+        pgcd=gmp_gcd($sup,$inf);
+        $sup=$sup/$pgcd;
+        $inf=$inf/$pgcd;
+    }while(gmp_gcd($sup,sinf)!=1);
+        
+    $bonne=($sup)." / ".($inf);
+	
+	$mauvaise_list=array(
+		0=>($rand * $rand2)."/".($rand1 * $rand3),
+		1=>,
+		2=>,
+		3=>);
+	shuffle($mauvaise_list);
+	
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//"4 par rapport à 12"
+function question2(){
+	$rand=rand(1,20);
+    $rand1=rand(1,20);
+    $enoce=($rand)." par rapport à ".($rand1*$rand);
+	    
+    $bonne= "est un diviseur";
+	
+	$mauvaise_list=array(
+		0=>"est un multiple",
+		1=>"est divisible",
+		2=>"est le PGCD",
+		3=>"premier");
+	shuffle($mauvaise_list);
+	
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//"Comment multiplie-t-on deux nombres rationnels ?"
+function question3(){
+	
+	$enoce="Comment multiplie-t-on deux nombres rationnels ?";
+	
+    $bonne="On multiplie les deux numérateurs entre eux et les deux dénominateurs entre eux";
+	
+	$mauvaise_list=array(
+		0=>"On multiplie le numérateur du premier avec le dénominateur du second et le numérateur du second avec le dénominateur du premier",
+		1=>"On additionne les deux numérateurs entre eux et on multiplie les deux dénominateurs entre eux",
+		2=>"On multiplie les deux dénominateurs entre eux et on additionne les deux dénominateurs entre eux",
+		3=>"On additionne les deux dénominateurs entre eux et on multiplie les deux dénominateurs entre eux");
+	shuffle($mauvaise_list);
+	
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//"Quel est l'inverse de 5/2 ?"
+function question4(){
+	$rand=rand(1,20);
+    $rand1=rand(1,20);
+    $enoce="Quel est l'inverse de ".($rand)."/".($rand1)." ?";
+	    
+    $bonne= ($rand1)."/".($rand).;
+	
+	$mauvaise_list=array(
+		0=>"-".($rand)."/".($rand1),
+		1="-".($rand)."/-".($rand1),
+		2=>"-".($rand1)."/".($rand),
+		3=>($rand1)*($rand));
+	shuffle($mauvaise_list);
+	
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//"Comment soustrait-on deux nombres relatifs ?"
+function question5(){
+	
+	$enoce="Comment multiplie-t-on deux nombres rationnels ?";
+	
+    $bonne="On multiplie les deux numérateurs entre eux et les deux dénominateurs entre eux";
+	
+	$mauvaise_list=array(
+		0=>"On multiplie le numérateur du premier avec le dénominateur du second et le numérateur du second avec le dénominateur du premier",
+		1=>"On additionne les deux numérateurs entre eux et on multiplie les deux dénominateurs entre eux",
+		2=>"On multiplie les deux dénominateurs entre eux et on additionne les deux dénominateurs entre eux",
+		3=>"On additionne les deux dénominateurs entre eux et on multiplie les deux dénominateurs entre eux");
+	shuffle($mauvaise_list);
+	
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//"PGCD(20,15)
+function question6(){
+	$rand=rand(1,100);
+    $rand1=rand(1,100);
+    $enoce="PGCD(".($rand).",".($rand1).") ?";
+	    
+    $bonne=gmp_gcd ($rand1,$rand).;
+	
+	$mauvaise_list=array(
+		0=>$bonne-rand(1,5),
+		1=>$bonne+rand(1,5),
+		2=>$bonne-rand(6,10),
+		3=>$bonne-rand(6,10));
+	shuffle($mauvaise_list);
+	
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//Comment simplifier 36/63
+function question7(){
+	$rand=rand(1,10);
+    $rand1=rand(1,10);
+	$rand2=rand(1,10);
+    $enoce="Comment simplifier".($rand*$rand2)."/".($rand1*$rand2)." ?";
+	    
+    $bonne=($rand)."/".($rand1).;
+	
+	$mauvaise_list=array(
+		0=>$bonne-rand(1,5),
+		1=>$bonne+rand(1,5),
+		2=>$bonne-rand(6,10),
+		3=>$bonne-rand(6,10));
+	shuffle($mauvaise_list);
+	
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+//"Lequel est divisible par 7 ?"
+function question8(){
+	$rand=rand(1,10);
+    $rand1=rand(1,10);
+	$rand2=rand(1,10);
+    $enoce=""Lequel est divisible par.($rand)." ?";
+	    
+    $bonne=($rand*$rand1).;
+	
+	$mauvaise_list=array(
+		0=>$bonne-rand(1,6),
+		1=>$bonne+rand(1,6),
+		2=>$bonne-rand(8,13),
+		3=>$bonne-rand(8,13));
+	shuffle($mauvaise_list);
+	
+	$bonne_rep=rand(0,3);
+	$retour=array();
+	for($i=0;$i<4;$i++){
+		if($i==$bonne_rep)
+			$retour[$i]=$bonne;
+		else
+            $retour[$i]=$mauvaise_list[$i];
+	}
+    
+
+	return array("enoce"=>$enoce,
+		"bonne_rep"=>$bonne_rep,
+		"reponce"=>$retour);
+}
+
+?>
