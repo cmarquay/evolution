ae592cc2daca8d9b8b7b6f106843173f9293d57f
Camille-Brillet <camille.brillet17@hotmail.com>
2016-03-13 12:57:44 +0100

    license et modifications classes quiz

--- classes_qcm/QCM.php
+++ classes_qcm/QCM.php
@@ -1,5 +1,26 @@
 <?php
 /*
+ * CC BY Project Brevet Evolution.
+ *
+ * Copyright CC BY 2016.
+ * Brillet Camille.
+ * Fillon Aurélien.
+ * Foissac Nicola.
+ * Hanotel Guillaume.
+ * Henry-Brûlé Raphaël.
+ * Marquay Christian.
+ *
+ * Creative Commons Attribution 3.0 France.
+ * http://creativecommons.org/licenses/by/3.0/fr/legalcode
+ *
+ */
+
+/**
+ *
+ * @author cbrillet
+ */
+
+/*
 
 Cette classe contiendra (temporairement?) toutes les mÃ©thodes de crÃ©ation de questions.
 Le tableau des appels sera shuffle et nbQuestions questions seront choisies
@@ -19,25 +40,31 @@ class QCM
     public function __construct($nbquest, $diff)
     {
         shuffle($this->_array_appels);
-        /*Le nombre de questions chiosies ne peuvent pas dÃ©passer du tableau*/
+        /*Le nombre de questions choisies ne peuvent pas dÃ©passer du tableau*/
         if ( $nbquest > count($this->_array_appels)) {
             $nbquest = count($this->_array_appels);
         }
         for ($i = 0; $i < $nbquest; $i++) {
             $func = $this->_array_appels[i];
             $params = call_user_func($func);
-            $this->_questionsPosees[i] = new Question($params["enonce"],$params["bonne_rep"],$params["reponses"]);
+            $this->_questionsPosees[i] = new Question($params["enonce"],$params["bonne_rep"],$params["reponses"],$this);
         }
-        $this->_quest_actuelle = 0;
+        $this->_questSuiv = 0;
+        $this->_nbQuestions = $nbquest;
+        $this->_nbPoints = 0;
     }
     
     public function nextQuestion()
     {
-        return $this->_questionsPosees[$this->_questSuiv++];
+        if ($this->_questSuiv < $this->_nbQuestions) {
+            return $this->_questionsPosees[$this->_questSuiv++];
+        } else {
+            return "Fin du Quiz";
+        }
     }
 
     /*Si la fonction estLaBonneReponse de Question renvoie true, +1 point*/
-    public static function unPoint($bonneRep) {
+    public function unPoint($bonneRep) {
         if ($bonneRep == true) {
             $this->_nbPoints++;
         }
--- classes_qcm/Question.php
+++ classes_qcm/Question.php
@@ -1,16 +1,38 @@
 <?php
+/*
+ * CC BY Project Brevet Evolution.
+ *
+ * Copyright CC BY 2016.
+ * Brillet Camille.
+ * Fillon Aurélien.
+ * Foissac Nicola.
+ * Hanotel Guillaume.
+ * Henry-Brûlé Raphaël.
+ * Marquay Christian.
+ *
+ * Creative Commons Attribution 3.0 France.
+ * http://creativecommons.org/licenses/by/3.0/fr/legalcode
+ *
+ */
+
+/**
+ *
+ * @author cbrillet
+ */
 
 class Question
 {
     private $_enonce;
     private $_reponses;
     private $_bonneRep;
+    private $_QCM;
 
-    public function __construct($enonce, $bonneRep, $reponses)
+    public function __construct($enonce, $bonneRep, $reponses, QCM $qcm)
     {
         $this->_enonce = $enonce;
         $this->_reponses = $reponses;
         $this->_bonneRep = $bonneRep;
+        $this->_QCM = $qcm;
     }
     
     public function estLaBonneReponse($numero)
@@ -20,7 +42,7 @@ class Question
         } else {
             $res = false;
         }
-        QCM::unPoint($res);
+        $this->_QCM->unPoint($res);
     }
     
     public function traitement($numero) {
@@ -39,10 +61,8 @@ class Question
             <input type='submit' name='traitement' value=".$this->_reponses[1]." onclick='traitement(1)' />
             <input type='submit' name='traitement' value=".$this->_reponses[2]." onclick='traitement(2)' />
             <input type='submit' name='traitement' value=".$this->_reponses[3]." onclick='traitement(3)' />
-        </form>;";
+        </form>";
         return $html;
-        
-        /*include les questions ici*/
         
     }
     
